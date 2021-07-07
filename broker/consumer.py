eb9957e7fede371c4a9883df6bcf0031441823e8
Miloš Radojčin <milos.radojcin.97@gmail.com>
2021-07-07 20:44:37 +0200

    Docker compose file (#15)

---
+++ broker/consumer.py
@@ -0,0 +1,23 @@
+import json
+# from service import user_service
+
+class UserConsumer:
+    def __init__(self, channel):
+        self.exchange_name = 'user'
+        self.channel = channel
+        channel.exchange_declare(exchange=self.exchange_name, exchange_type='fanout')
+        q = channel.queue_declare(queue='')
+        channel.queue_bind(exchange=self.exchange_name, queue=q.method.queue)
+        channel.basic_consume(queue=q.method.queue, on_message_callback=self.on_message, auto_ack=True)
+
+    def on_message(self, ch, method, properties, body):
+        try:
+            data = json.loads(body)
+
+            # if properties.content_type == 'user.created': 
+            #     user_service.add(data)
+            # elif properties.content_type == 'user.deleted':
+            #     user_service.delete(data['id'])
+        except Exception:
+            # don't crash
+            pass
