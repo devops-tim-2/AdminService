becffb67b9f6ca37f1f65a94967be7e66275390f
Miloš Radojčin <milos.radojcin.97@gmail.com>
2021-07-07 20:41:48 +0200

    Docker compose file

---
+++ common/config.py
@@ -0,0 +1,46 @@
+from os import environ
+from flask_cors import CORS
+from flask.app import Flask
+from flask_wtf import CSRFProtect
+from flask_restful import Api
+
+config = {
+    'test': 'TEST_DATABASE_URI',
+    'dev': 'DEV_DATABASE_URI'
+}
+
+def setup_config(cfg_name: str):
+    environ['SQLALCHEMY_DATABASE_URI'] = environ.get(config[cfg_name])
+    
+    app = Flask(__name__)
+    if environ.get('ENABLE_CSRF') == 1:
+        app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
+        app.config['WTF_CSRF_SECRET_KEY'] = environ.get('WTF_CSRF_SECRET_KEY')
+        csrf = CSRFProtect()
+        csrf.init_app(app)
+        
+    CORS(app, resources={r"/*": {"origins": "http://localhost:3000", "send_wildcard": "False"}})
+    api = Api(app)
+
+
+    # This import must be postponed because importing common.database has side-effects
+    from common.database import init_db
+    init_db()
+
+
+    
+    # This import must be postponed after init_db has been called
+    # from controller.campaign_controller import CampaignResource, PostCampaignResource, UserResource
+    # api.add_resource(CampaignResource, '/api/<campaign_id>')
+    # api.add_resource(PostCampaignResource, '/api')
+    # api.add_resource(UserResource, '/api/profile/<user_id>')
+
+
+    # This import must be postponed after init_db has been called
+    # from models.models import Campaign, CampaignActivation, User
+    # if cfg_name == 'test':
+    #     CampaignActivation.query.delete()
+    #     Campaign.query.delete()
+    #     User.query.delete()
+
+    return app
